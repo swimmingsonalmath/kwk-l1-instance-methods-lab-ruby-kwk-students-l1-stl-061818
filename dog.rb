49a7c4554ef733d00ec7f7f6e4fc2db419b3da91
Sonali Sharma <sonalisharma58521@gmail.com>
2018-06-22 17:35:07 +0000

    Done.

---
+++ dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  def initialize 
+  end
+
+def bark
+  puts "Woof!"
+  end
+
+def sit 
+  puts "The Dog is sitting"
+end 
+
+end 
+
+billy =Dog.new
+billy.bark
+billy.sit 
+  
+    
---
+++ person.rb
@@ -0,0 +1,18 @@
+class Person
+  def initialize
+end
+
+def talk
+  puts "Hello World"
+end
+
+def walk
+  puts "The Person is walking"
+  
+end
+
+end
+
+sonali= Person.new 
+sonali.talk
+sonali.walk
